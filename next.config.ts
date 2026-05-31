a3c5b7875d0fe6298cb9e80fab707fb774f596ff
Kashish08-gif <kashish240130@gmail.com>
2026-05-31 17:42:42 +0530

    Resolve merge conflicts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-<<<<<<< HEAD
-  /* config options here */
-=======
   turbopack: {
     root: __dirname,
   },
->>>>>>> 3df9883b6716181974921f8f1017d13bee301f52
 };
 
 export default nextConfig;
